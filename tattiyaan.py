d0427ae53e49e7b3a5920a22d130e0fb0218c15b
Dean Hunter <jeeka7@gmail.com>
2022-05-29 22:14:10 +0530

    Update tattiyaan.py

--- tattiyaan.py
+++ tattiyaan.py
@@ -8,6 +8,7 @@ st.write("Here our attempt at using data to create a table")
 
 a = st.slider('Enter Value of a',-10,20,0)
 b = st.slider('Enter Value of b',-10,20,0)
+st.write('(a+b) is', (a+b))
 st.write('(a+b) whole square is', (a+b)**2)
 st.write('(a-b) whole square is', (a-b)**2)
 st.write('(a+b) whole cube is', (a+b)**3)
