8fa5ec7716e1fab030ee046089f7a98ccb1d8cda
Dean Hunter <jeeka7@gmail.com>
2022-05-29 23:01:32 +0530

    Update mathsactivity.py

--- mathsactivity.py
+++ mathsactivity.py
@@ -14,7 +14,7 @@ st.write('(a-b)\u00b2 =', (a-b)**2)
 st.write('(a+b)\u00b3 =', (a+b)**3)
 st.write('(a-b)\u00b3 =', (a-b)**3)
 
-if st.button('Click this if you are happy'):
+if st.button('Click this if you are happy OR'):
      st.write('Good for you , go make others happy ')
 else:
      st.write('Start doing what makes you happy')
