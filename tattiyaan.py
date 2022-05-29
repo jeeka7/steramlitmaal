f6804b776fa746d0aaf10373c54269a74bd8d1b3
Dean Hunter <jeeka7@gmail.com>
2022-05-29 17:10:08 +0530

    Update tattiyaan.py

--- tattiyaan.py
+++ tattiyaan.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.write("Here our attempt at using data to create a table")
 st.write(pd.DataFrame({
 'first column' : [1,2,3,4],
  'second column' : [10,20,30,40]
 }))
+
+
+df = pd.DataFrame(
+ np.random.randn(10,20),
+ columns= ('col %d' % i for i in range (20)))
+
+st.df(df.style.highlight_max(axis=0))
